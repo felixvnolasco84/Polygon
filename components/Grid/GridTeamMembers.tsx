39fe9c4b8efc5641c1af030d44dcea8e304ec6d8
felixvnolasco84 <felix@polygonag.com>
2023-11-27 15:08:11 -0600

    :tada:

--- components/Grid/GridTeamMembers.tsx
+++ components/Grid/GridTeamMembers.tsx
@@ -2,10 +2,12 @@ import { neueXThin, neueThin } from "@/fonts";
 
 const GridTeamMembers = ({ items }: any) => {
   return (
-    <div className="grid grid-cols-3 gap-x-4 gap-y-8 px-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:gap-x-4 lg:gap-y-24 lg:px-24 xl:grid-cols-5">
-      {items.map((item: any, index: any) => (
-        <div className="flex flex-col gap-[30px]" key={index}>
-          <div className="min-h-[145px] w-full rounded-[10px] bg-white p-4 lg:h-[371px] lg:w-[313px] lg:max-w-none"></div>
+    <div className={`${neueLight.className} flex flex-col gap-4 px-2 lg:px-24`}>
+      <h4 className="text-2xl text-black-400">Nuestro equipo.</h4>
+      <div className="grid grid-cols-3 gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:gap-x-4 lg:gap-y-24 lg:px-24 xl:grid-cols-5">
+        {items.map((item: any, index: any) => (
+          <div className="flex flex-col gap-[30px]" key={index}>
+            <div className="min-h-[145px] w-full rounded-[10px] bg-white p-4 lg:h-[371px] lg:w-[313px] lg:max-w-none"></div>
           <div className="flex flex-col gap-[10px]">
             <span
               className={`${neueThin.className} text-sm lg:text-2xl text-black-500`}
