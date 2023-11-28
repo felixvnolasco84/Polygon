610c9149203fef3e9a6bdb462883c16f533cb149
felixvnolasco84 <felix@polygonag.com>
2023-11-28 12:17:25 -0600

    :tada:

--- app/productos/[id]/page.tsx
+++ app/productos/[id]/page.tsx
@@ -6,20 +6,46 @@ import { useState } from "react";
 import { Slider } from "@/components/ui/slider";
 import { QuoteForm } from "@/components/Interest/QuoteForm";
 import { services } from "@/components/Grid/GridServices";
+import { Metadata } from "next";
+import { object } from "zod";
 
 export default function Page({ params }: any) {
   const { id } = params;
 
   const service: any = services.find((service: any) => service.slug === id);
   console.log(service);
-  
+
+  //   const metadata: Metadata = {
+  //   title: service.pageTitle,
+  //   description: service.metadescription,
+  // };
+
   return (
     <>
       <div className="bg-[url('https://res.cloudinary.com/dirtcb7dj/image/upload/v1695236636/Hero/zqgdrhztqkjuxsadesjb.png')] bg-cover bg-center bg-no-repeat">
         <div className="flex h-[1024px] flex-col justify-end gap-4 bg-gradient-to-b from-transparent to-[#00000085] px-8 pb-[30px] lg:px-24">
+          <h1 className={`${neueXThin.className} text-7xl text-white`}>
+            {service.title}
+          </h1>
+          <div className="flex items-start gap-4">
+            {
+              service?.objectives.map((objective: any, index: any) => (
+              <div className="flex flex-col gap-4">
+                <p
+                  className={`${neueThin.className} text-2xl text-white lg:leading-[35px]`}
+                >
+                  {objective.title}
+                </p>
 
-          <h1 className={`${neueXThin.className} text-7xl text-white`}>{service.title}</h1>
-          
+                <p
+                  className={`${neueLight.className} text-xl text-white lg:leading-[35px]`}
+                >
+                  {objective.description}
+                </p>
+              </div>
+              ))
+            }
+          </div>
           <div className="block border-t-4 border-white pt-10 lg:hidden">
             {service?.features.slice(1, 2).map((feature: any, index: any) => (
               <div
