a425d5980ab671c932b4e4d433d3d203dcffaaee
felixvnolasco84 <felix@polygonag.com>
2023-12-05 14:46:03 -0600

    Commented out unused code in customer and quote routes

--- app/api/quotes/route.ts
+++ app/api/quotes/route.ts
@@ -1,20 +1,20 @@
-import { NextResponse } from "next/server";
-import prisma from "@/utils/ConnectionPool";
+// import { NextResponse } from "next/server";
+// import prisma from "@/utils/ConnectionPool";
 
-// CREATE A QUOTE
-export const POST = async (req: any) => {
-  try {
-    const body = await req.json();
-    console.log(body);
-    // const quote = await prisma.quote.create({
-    //   data: { ...body },
-    // });
+// // CREATE A QUOTE
+// export const POST = async (req: any) => {
+//   try {
+//     const body = await req.json();
+//     console.log(body);
+//     // const quote = await prisma.quote.create({
+//     //   data: { ...body },
+//     // });
 
-    // return new NextResponse(JSON.stringify(quote));
-  } catch (err) {
-    console.log(err);
-    return new NextResponse(
-      JSON.stringify({ message: "Something went wrong!" })
-    );
-  }
-};
+//     // return new NextResponse(JSON.stringify(quote));
+//   } catch (err) {
+//     console.log(err);
+//     return new NextResponse(
+//       JSON.stringify({ message: "Something went wrong!" })
+//     );
+//   }
+// };
