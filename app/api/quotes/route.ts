b4a3e791731527ae8ebab75e58de6f01d38060b9
felixvnolasco84 <felix@polygonag.com>
2023-12-05 16:54:04 -0600

    Refactor POST method to create a quote

--- app/api/quotes/route.ts
+++ app/api/quotes/route.ts
@@ -4,13 +4,12 @@ import prisma from "@/utils/ConnectionPool";
 // CREATE A QUOTE
 export const POST = async (req: any) => {
   try {
-    const body = await req.json();
-    console.log(body);
-    // const quote = await prisma.quote.create({
-    //   data: { ...body },
-    // });
-
-    // return new NextResponse(JSON.stringify(quote));
+    const body = await req.json();    
+    const quote = await prisma.quote.create({
+      data: { ...body },
+    });
+    console.log(quote);
+    return new NextResponse(JSON.stringify(quote));
   } catch (err) {
     console.log(err);
     return new NextResponse(
