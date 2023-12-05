41762bcef36ec7ed22e957c3902f877f283a55c2
felixvnolasco84 <felix@polygonag.com>
2023-12-05 14:50:06 -0600

    Refactor API route files

--- app/api/quotes/route.ts
+++ app/api/quotes/route.ts
@@ -1,22 +1,21 @@
-// import { NextResponse } from "next/server";
-// import prisma from "@/utils/ConnectionPool";
+import { NextResponse } from "next/server";
+import prisma from "@/utils/ConnectionPool";
 
-// // CREATE A QUOTE
-// export const POST = async (req: any) => {
-//   try {
-//     const body = await req.json();
-//     console.log(body);
-//     // const quote = await prisma.quote.create({
-//     //   data: { ...body },
-//     // });
+// CREATE A QUOTE
+export const POST = async (req: any) => {
+  try {
+    const body = await req.json();
+    console.log(body);
+    // const quote = await prisma.quote.create({
+    //   data: { ...body },
+    // });
 
-//     // return new NextResponse(JSON.stringify(quote));
-//   } catch (err) {
-//     console.log(err);
-//     return new NextResponse(
-//       JSON.stringify({ message: "Something went wrong!" })
-//     );
-//   }
-// };
+    // return new NextResponse(JSON.stringify(quote));
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" })
+    );
+  }
+};
 
-const a = ""
