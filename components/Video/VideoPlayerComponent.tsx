780332eaf4b6dbcbcc463f38bf75e0e8c86649cf
felixvnolasco84 <felix@polygonag.com>
2024-01-09 23:32:43 -0600

    Add loading animation and text to VideoPlayerComponent

--- components/Video/VideoPlayerComponent.tsx
+++ components/Video/VideoPlayerComponent.tsx
@@ -1,3 +1,4 @@
+import { neueXThin } from "@/styles/fonts";
 import { useState } from "react";
 import ReactPlayer from "react-player";
 
@@ -5,19 +6,37 @@ export function ReactPlayerAsVideo(props: any) {
   const [isPlaying, setisPlaying] = useState(true);
   const [isLoading, setisLoading] = useState(false);
 
-  let { asset, src, poster, blurDataURL, ...rest } = props;
-  let config = { file: { attributes: { poster } } };
+  let { src, ...rest } = props;
 
   return (
     <div className="h-full w-full" onClick={() => setisPlaying(!isPlaying)}>
       {true && (
-      // {!isLoading && (
-        <div className="absolute h-full w-full animate-pulse bg-gray-400"></div>
-        // <img
-        //   className="absolute h-full w-full object-cover object-center"
-        //   src={blurDataURL}
-        //   alt=""
-        // />
+        // {!isLoading && (
+        <div className="absolute h-full w-full animate-pulse bg-gray-400">
+          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform">
+            <svg
+              className="h-12 w-12 animate-spin text-[#edff00]"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+              ></path>
+            </svg>
+            <p className={`${neueXThin.className} mt-2 text-sm text-black-400`}>Cargando...</p>
+          </div>
+        </div>
       )}
       <ReactPlayer
         onReady={() => setisLoading(true)}
@@ -26,7 +45,6 @@ export function ReactPlayerAsVideo(props: any) {
         loop={true}
         muted={true}
         url={src}
-        config={config}
         // ref={videoEl}
         {...rest}
       />
