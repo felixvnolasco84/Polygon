c432bd918e03606f03573646c0cb43de4a64706c
felixvnolasco84 <felix@polygonag.com>
2024-01-09 23:24:33 -0600

    Refactor VideoComponent and VideoPlayerComponent

--- components/Video/VideoComponent.tsx
+++ components/Video/VideoComponent.tsx
@@ -4,8 +4,8 @@ import { ReactPlayerAsVideo } from "@/components/Video/VideoPlayerComponent";
 export default function VideoComponent({ video }: { video: any }) {
   return (
     <Video      
-      as={ReactPlayerAsVideo}                  
-      className="absolute h-full w-full"
+      as={ReactPlayerAsVideo}                        
+      className="absolute h-full w-full"      
       controls={false}
       src={video}     
     />
--- components/Video/VideoPlayerComponent.tsx
+++ components/Video/VideoPlayerComponent.tsx
@@ -3,13 +3,24 @@ import ReactPlayer from "react-player";
 
 export function ReactPlayerAsVideo(props: any) {
   const [isPlaying, setisPlaying] = useState(true);
+  const [isLoading, setisLoading] = useState(false);
 
   let { asset, src, poster, blurDataURL, ...rest } = props;
   let config = { file: { attributes: { poster } } };
 
   return (
     <div className="h-full w-full" onClick={() => setisPlaying(!isPlaying)}>
+      {true && (
+      // {!isLoading && (
+        <div className="absolute h-full w-full animate-pulse bg-gray-400"></div>
+        // <img
+        //   className="absolute h-full w-full object-cover object-center"
+        //   src={blurDataURL}
+        //   alt=""
+        // />
+      )}
       <ReactPlayer
+        onReady={() => setisLoading(true)}
         playing={isPlaying}
         playsinline={true}
         loop={true}
