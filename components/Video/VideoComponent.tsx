8ad3bc9f854d22d999ef816d6d4226cc10e42f1e
felixvnolasco84 <felix@polygonag.com>
2024-01-09 23:03:33 -0600

    Fix video object-fit issue and update video player component

--- components/Video/VideoComponent.tsx
+++ components/Video/VideoComponent.tsx
@@ -1,23 +1,13 @@
 import Video from "next-video";
-import { useState } from "react";
 import { ReactPlayerAsVideo } from "@/components/Video/VideoPlayerComponent";
 
 export default function VideoComponent({ video }: { video: any }) {
-  const [isPaused, setIsPaused] = useState(false);
-  console.log(isPaused);
-
   return (
-    <div className="h-full" onClick={() => setIsPaused(!isPaused)}>
-      <Video
-        minResolution="1080p"
-        accentColor="#edff00"                
-        // as={ReactPlayerAsVideo}
-        loop={true}
-        autoPlay={true}
-        controls={true}
-        className="h-full w-full"
-        src={video}
-      />
-    </div>
+    <Video      
+      as={ReactPlayerAsVideo}                  
+      className="absolute h-full w-full"
+      controls={false}
+      src={video}     
+    />
   );
 }
--- components/Video/VideoPlayerComponent.tsx
+++ components/Video/VideoPlayerComponent.tsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
 import ReactPlayer from "react-player";
 
 export function ReactPlayerAsVideo(props: any) {
+  const [isPlaying, setisPlaying] = useState(true);
+
   let { asset, src, poster, blurDataURL, ...rest } = props;
   let config = { file: { attributes: { poster } } };
 
-  return <ReactPlayer url={src} {...rest} />;
+  return (
+    <div className="h-full w-full" onClick={() => setisPlaying(!isPlaying)}>
+      <ReactPlayer
+        playing={isPlaying}
+        playsinline={true}
+        loop={true}
+        muted={true}
+        url={src}
+        config={config}
+        // ref={videoEl}
+        {...rest}
+      />
+    </div>
+  );
 }
