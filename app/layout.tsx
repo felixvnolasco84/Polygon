4acbf1635508c6c5f2c319cf14ce3b4a2dc276d7
Felix Vega <felix@polygonag.com>
2024-02-02 15:30:22 -0600

    added google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import { Toaster } from "@/components/ui/toaster";
 import { Metadata } from "next";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title:
@@ -20,13 +21,34 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KGCL4DFS')`,
+          }}
+        >
+          {" "}
+        </script>
+      </head>
       <body className={`container hide-scrollbar-utility`}>
         <Header />
-        <div className="flex-1">{children}</div>
+        <div className="flex-1">
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KGCL4DFS"
+            height="0"
+            width="0"
+          ></iframe>
+          {children}
+        </div>
         <SpeedInsights />
         <Analytics />
         <Toaster />
         <Footer />
+        <Script></Script>
       </body>
     </html>
   );
