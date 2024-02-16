5a864d5a24dc7ddce3df8f5047d12da9d40829ed
Felix Vega <felix@polygonag.com>
2024-02-16 13:18:57 -0600

    Update GridHomeFeaturesProps with minWidth and minHeight

--- components/Grid/GridHomeFeatures.tsx
+++ components/Grid/GridHomeFeatures.tsx
@@ -9,8 +9,8 @@ type GridHomeFeaturesProps = {
   image: StaticImageData;
   title: string;
   description: string;
-  maxWidth?: string;
-  maxHeight?: string;
+  minWidth?: string;
+  minHeight?: string;
 };
 
 const GridHomeFeaturesItems: GridHomeFeaturesProps[] = [
@@ -19,25 +19,31 @@ const GridHomeFeaturesItems: GridHomeFeaturesProps[] = [
     title: "Conecta y cautiva",
     description:
       "Creamos estrategias que fomentan la interacción con tu audiencia, generando una comunidad comprometida y fiel a tu marca.",
+    minWidth: "min-w-[159px]",
+    minHeight: "min-h-[216px]",
   },
   {
     image: ChartImage,
     title: "Maximiza tu inversión",
     description:
       "Nuestro enfoque estratégico garantiza que cada peso invertido genere un retorno óptimo, aumentando la eficiencia de tus campañas.",
+    minWidth: "min-w-[241px]",
+    minHeight: "min-h-[216px]",
   },
   {
     image: ComputerImage,
     title: "Haz que cada clic cuente",
     description:
       "Optimizamos la experiencia del usuario en tu sitio web, lo que se traduce en tasas de conversión más altas y clientes más satisfechos. fiel a tu marca.",
+    minWidth: "min-w-[262px]",
+    minHeight: "min-h-[193px]",
   },
 ];
 
 export default function GridHomeFeatures() {
   return (
     <div
-      className={`${neueXThin.className} flex flex-col lg:flex-row lg:gap-6 xl:gap-12 px-4 text-center lg:px-12 xl:px-24 items-center`}
+      className={`${neueXThin.className} flex flex-col lg:flex-row lg:gap-6 xl:gap-12 px-4 text-center lg:px-12 xl:px-24 items-baseline`}
     >
       {GridHomeFeaturesItems.map((item, index) => (
         <div
@@ -45,7 +51,7 @@ export default function GridHomeFeatures() {
           className="flex w-full flex-col items-center justify-center lg:w-4/12 lg:gap-1 xl:gap-3"
         >
           <div
-            className={`relative flex aspect-square w-8/12 max-w-[216px] justify-center md:w-4/12 lg:w-full`}
+            className={`${item.minWidth} ${item.minHeight} relative flex aspect-auto w-8/12 max-w-[216px] justify-center md:w-4/12 lg:w-full`}
           >
             <Image
               className={`h-full w-full rounded-[10px] object-fill object-center`}
