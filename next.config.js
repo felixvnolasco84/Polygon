a4e97c4a3cb130eaab92574c94d69982398bdf13
Felix Vega <felix@polygonag.com>
2024-02-29 17:05:16 -0600

    Add redirects and headers configuration to next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,71 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com", "firebasestorage.googleapis.com"],
   },
-  
+  async redirects() {
+    return [
+      {
+        source: "https://polygonag.com/apetito.html",
+        destination: "https://www.polygonag.com/",
+        permanent: false, // Esto es una redirección permanente (301)
+      },
+      {
+        source: "https://www.polygonag.com/tuborg.html",
+        destination: "https://www.polygonag.com/",
+        permanent: false, // Esto es una redirección temporal (302)
+      },
+      {
+        source: "http://www.polygonag.com/apetito.html",
+        destination: "https://www.polygonag.com/",
+        permanent: false, // Esto es una redirección temporal (302)
+      },
+      {
+        source: "https://www.polygonag.com/actinver.html",
+        destination: "https://www.polygonag.com/",
+        permanent: false, // Esto es una redirección temporal (302)
+      },
+      {
+        source: "http://www.polygonag.com/copa.html",
+        destination: "https://www.polygonag.com/",
+        permanent: false, // Esto es una redirección temporal (302)
+      },
+      {
+        source: "http://polygonag.com/copa.html",
+        destination: "https://www.polygonag.com/",
+        permanent: false, // Esto es una redirección temporal (302)
+      },
+      {
+        source: "https://polygonag.com/contacto.html",
+        destination: "https://www.polygonag.com/",
+        permanent: true, // Esto es una redirección temporal (302)
+      },
+      {
+        source: "https://polygonag.com/nosotros.html",
+        destination: "https://www.polygonag.com/",
+        permanent: true, // Esto es una redirección temporal (302)
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: "https://www.polygonag.com/index.html",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "text/plain",
+          },
+          {
+            key: "Content-Length",
+            value: "0",
+          },
+          {
+            key: "Status",
+            value: "410 Gone",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = withNextVideo(nextConfig);
