9c5d0937ee043729d67c43b85fc1dfbd03a5d965
Felix Vega <felix@polygonag.com>
2024-02-29 15:25:01 -0600

    Remove unused rewrites in next.config.js

--- next.config.js
+++ next.config.js
@@ -6,14 +6,7 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com", "firebasestorage.googleapis.com"],
   },
-  async rewrites() {
-    return [
-      {
-        source: "/:path*",
-        destination: "/",
-      },
-    ];
-  },
+  
 };
 
 module.exports = withNextVideo(nextConfig);
