7caf02ed1deeb74ca7c4027fa3cc3c96c6f33e71
Felix Vega <felix@polygonag.com>
2024-03-21 11:28:54 -0600

    Refactor Header component layout

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -8,21 +8,23 @@ import PolygonLogo from "../Logo/PolygonLogo";
 
 export default function Header() {
   return (
-    <div className="sticky top-0 z-40 flex h-16 w-full items-center justify-between bg-gray-400 px-4 lg:h-20 xl:h-[104px]">
-      <Link
-        className="min-h-[22px] max-w-[90px] md:max-w-[110px] lg:max-w-[160px] xl:max-w-[180px]"
-        href={"/"}
-      >
-        {/* <Image className="text-flourescentYellow" src={PolygonIcon} alt="" width={209} height={42} /> */}
-        <PolygonLogo fillColor="" />
-      </Link>
-      <div
-        className={`${neueLight.className} hidden lg:flex text-2xl gap-[55px] items-center`}
-      >
-        <ContactDialog type="header" />
-      </div>
-      <div className="block lg:hidden">
-        <ContactDialog type="header" />
+    <div className="sticky top-0 z-40 w-full bg-gray-400">
+      <div className="container flex h-16 items-center justify-between px-4 lg:h-20 xl:h-[104px]">
+        <Link
+          className="min-h-[22px] max-w-[90px] md:max-w-[110px] lg:max-w-[160px] xl:max-w-[180px]"
+          href={"/"}
+        >
+          {/* <Image className="text-flourescentYellow" src={PolygonIcon} alt="" width={209} height={42} /> */}
+          <PolygonLogo fillColor="" />
+        </Link>
+        <div
+          className={`${neueLight.className} hidden lg:flex text-2xl gap-[55px] items-center`}
+        >
+          <ContactDialog type="header" />
+        </div>
+        <div className="block lg:hidden">
+          <ContactDialog type="header" />
+        </div>
       </div>
     </div>
   );
