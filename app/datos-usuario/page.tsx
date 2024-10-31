264a1d4607bf02fced5878139b372a8a28b22f70
felixvnolasco <felix@polygonag.com>
2024-10-31 14:23:46 -0600

    added new page

---
+++ app/datos-usuario/page.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <p>
+        Política de Eliminación de Datos de Bold AI En Bold AI, respetamos tu
+        derecho a la privacidad y a la gestión de tus datos personales. Nos
+        comprometemos a eliminar tus datos personales de nuestros sistemas si
+        así lo solicitas. 1. Derecho a la Eliminación de Datos Tienes el derecho
+        de solicitar la eliminación de tus datos personales en cualquier
+        momento. Esto incluye la información de tu cuenta, datos de Facebook
+        Ads, y cualquier otro dato relacionado almacenado en nuestra plataforma.
+        2. Proceso de Solicitud de Eliminación de Datos Para solicitar la
+        eliminación de tus datos personales: • Envía una solicitud a través de
+        nuestro formulario de eliminación de datos o contáctanos a través de
+        [correo electrónico de contacto]. • Asegúrate de incluir la información
+        necesaria para verificar tu identidad y tu cuenta, como el nombre
+        completo, correo electrónico asociado, y, si corresponde, el ID de la
+        cuenta. 3. Confirmación de Eliminación Una vez que recibamos tu
+        solicitud, nuestro equipo la procesará dentro de los siguientes 30 días
+        hábiles. Te enviaremos una confirmación cuando tus datos hayan sido
+        eliminados permanentemente de nuestros sistemas. 4. Consecuencias de la
+        Eliminación de Datos Ten en cuenta que una vez que tus datos sean
+        eliminados, no podremos restaurarlos. Esta acción es irreversible y
+        podría afectar tu capacidad de usar nuestra plataforma o acceder a
+        ciertos servicios que dependen de la información eliminada. 5.
+        Excepciones En algunos casos, es posible que estemos obligados a
+        conservar ciertos datos por motivos legales, regulatorios o de
+        cumplimiento, como el mantenimiento de registros comerciales o para
+        cumplir con leyes y regulaciones aplicables. En tales casos, se te
+        informará de los datos que no pueden ser eliminados y las razones
+        correspondientes. Contáctanos Si tienes alguna pregunta o deseas
+        solicitar la eliminación de tus datos, por favor contáctanos en [correo
+        electrónico de contacto]. Compromiso de Bold AI Estamos comprometidos a
+        garantizar que todos los usuarios puedan ejercer su derecho a la
+        eliminación de datos y gestionamos todas las solicitudes de manera
+        segura, confidencial y de acuerdo con las normativas de privacidad
+        aplicables.
+      </p>
+    </div>
+  );
+}
