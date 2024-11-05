a7f48c63ad195ade73128d571e446ba7a1532750
felixvnolasco <felix@polygonag.com>
2024-11-04 23:08:06 -0600

    added twilio route

---
+++ app/api/finsolar/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+
+const twilio = require("twilio");
+
+export const POST = async (req: any) => {
+  try {
+    //SEND WHATSAPP MESSAGE
+    const accountSID = process.env.TWILIO_ACCOUNT_SID;
+    const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+    const client = new twilio(accountSID, authToken);
+
+    console.log(req.body);
+
+    // Envía el mensaje
+    client.messages
+      .create({
+        body: "¡Hola! Este es un mensaje enviado desde la API de WhatsApp de Twilio.",
+        from: "whatsapp:+525578136020", // Este es un número de WhatsApp de Twilio
+        to: "whatsapp:+525578136020", // Reemplázalo con el número de WhatsApp del destinatario
+      })
+      .then((message: any) => console.log("Mensaje enviado con SID: " + message.sid))
+      .catch((error: any) => console.error("Error al enviar mensaje: ", error));
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" })
+    );
+  }
+};
